06a50ae87f9b263a0a797481caa5465fe435e1f3
Runone <ngo.danny11@gmail.com>
2018-10-10 20:21:28 -0700

    first commit

--- server/schema.sql
+++ server/schema.sql
@@ -2,12 +2,32 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+/* Create other tables and define schemas for them here! */
+CREATE TABLE usernames (
   /* Describe your table here.*/
+  id integer NOT NULL AUTO_INCREMENT,
+  username text,
+  PRIMARY KEY (id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id integer NOT NULL AUTO_INCREMENT,
+  room text,
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id integer NOT NULL AUTO_INCREMENT,
+  message text,
+  createdAt date,
+  username integer,
+  room integer,
+  PRIMARY KEY (id),
+  FOREIGN KEY (username) REFERENCES usernames(id),
+  FOREIGN KEY (room) REFERENCES rooms(id)
+);
 
 
 
